95cfe7e90b9a31262d2751e1275b21ee96d4c44c
Đức Đào <duc.dao.431@gmail.com>
2023-03-13 20:47:07 +0700

    ✨ feat(conf): specify port for vite

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  	plugins: [react()],
+	server: {
+		host: "192.168.1.10",
+		port: 6996,
+	}
 })
