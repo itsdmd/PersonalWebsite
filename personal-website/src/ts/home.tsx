a34b0c29f6fda96774778a1ce5a302a85b2e1926
Đức Đào <duc.dao.431@gmail.com>
2023-03-14 10:18:51 +0700

    🦄 refactor(home): update font source

--- personal-website/src/ts/home.tsx
+++ personal-website/src/ts/home.tsx
@@ -5,7 +5,15 @@ function Home() {
 	return (
 		<div className="Home">
 			<div className="bg-gray-dark h-screen">
-				<div className="flex justify-center">
+				<div className="flex justify-center" id="header">
+					<div className="flex justify-between items-center h-18 m-4 w-11/12">
+						<div className="flex items-center">
+							<h1 className="font-black font-display text-5xl text-white mr-8">D</h1>
+							<h1 className="font-display text-3xl text-white -translate-y-0.5">itsdmd</h1>
+						</div>
+					</div>
+				</div>
+				<div className="flex justify-center" id="greeting-card">
 					<div className="bg-gray drop-shadow-2xl flex h-96 items-center justify-center max-w-5xl mt-20 rounded-3xl w-11/12">
 						<div className="translate-y-4">
 							<h1 className="font-bold font-monospace shadow-gray-light text-7xl text-center text-shadow-lg text-white">
