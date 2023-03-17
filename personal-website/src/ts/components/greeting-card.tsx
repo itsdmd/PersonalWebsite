67469f4257cdc62bf4304ddc137888eb8a3b3f76
Đức Đào <duc.dao.431@gmail.com>
2023-03-17 15:05:39 +0700

    ✨ feat(home): add shadow for greeting card

--- personal-website/src/ts/components/greeting-card.tsx
+++ personal-website/src/ts/components/greeting-card.tsx
@@ -4,7 +4,13 @@ import { MdWavingHand } from "react-icons/md";
 
 function greetingCard() {
 	return (
-		<div className="_greeting-card-wrapper flex justify-center">
+		<div className="_greeting-card-wrapper flex justify-center relative">
+			<motion.div
+				className="_greeting-card-shadow absolute bg-gray blur-xl drop-shadow-2xl flex h-40 items-center justify-center max-w-[66rem] md:h-96 md:mt-[22rem] mt-64 rounded-3xl sm:h-60 w-11/12"
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.5, duration: 2, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
+			></motion.div>
 			<motion.div
 				className="_greeting-card bg-gray drop-shadow-2xl flex h-40 items-center justify-center max-w-5xl md:h-96 md:mt-80 mt-60 rounded-3xl sm:h-60 w-11/12"
 				initial={{ y: "2.5rem" }}
@@ -13,7 +19,7 @@ function greetingCard() {
 			>
 				<div className="translate-y-4">
 					<motion.h1
-						className="font-bold font-monospace md:text-7xl shadow-gray-light sm:text-5xl text-4xl text-center text-shadow-lg text-white"
+						className="_greeting-card-text font-bold font-monospace md:text-7xl shadow-gray-light sm:text-5xl text-4xl text-center text-shadow-lg text-white"
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						transition={{ delay: 0.2, duration: 0.5 }}
