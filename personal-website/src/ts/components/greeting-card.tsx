652538a7031e02e4aeeae30f89d20f0b60f7e545
Đức Đào <duc.dao.431@gmail.com>
2023-04-01 15:50:02 +0700

    🎈 perf(home/gc): remove animation

--- personal-website/src/ts/components/greeting-card.tsx
+++ personal-website/src/ts/components/greeting-card.tsx
@@ -2,14 +2,12 @@ import { motion } from "framer-motion";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-import IcRoundWavingHand from "~icons/ic/round-waving-hand";
-
 const greetingCardPortraitImg = () => (
 	<motion.div
 		className="lg:translate-y-[8.5rem] md:translate-y-[7rem] sm:translate-y-[5rem] translate-y-[3rem] lg:scale-100 xl:scale-110 md:scale-95 scale-90"
-		initial={{ opacity: 0 }}
-		animate={{ opacity: 1 }}
-		transition={{ delay: 0.5, duration: 0.5 }}
+		// initial={{ opacity: 0 }}
+		// animate={{ opacity: 1 }}
+		// transition={{ delay: 0.5, duration: 0.5 }}
 	>
 		<LazyLoadImage src="/src/assets/img/self-portrait.webp" alt="Portrait of Minh Duc Dao" effect="blur" visibleByDefault={true} />
 	</motion.div>
@@ -20,15 +18,15 @@ function greetingCard() {
 		<div className="_greeting-card-wrapper flex justify-center relative">
 			<motion.div
 				className="_greeting-card-shadow absolute bg-gray-300 blur-xl dark:bg-d-bg drop-shadow-2xl flex h-32 items-center justify-center lg:h-80 max-w-[82rem] md:h-64 md:mt-[21rem] md:rounded-3xl mt-[16rem] rounded-xl sm:h-52 w-11/12 xl:h-96"
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ delay: 0.5, duration: 2, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
+				// initial={{ opacity: 0 }}
+				// animate={{ opacity: 1 }}
+				// transition={{ delay: 0.5, duration: 2, repeat: Infinity, repeatType: "reverse", ease: "easeInOut" }}
 			></motion.div>
 			<motion.div
 				className="_greeting-card bg-l-bg dark:bg-d-bg drop-shadow-2xl flex h-32 items-center justify-start lg:h-80 max-w-7xl md:h-64 md:mt-80 md:px-4 md:rounded-3xl mt-60 rounded-xl sm:h-52 sm:px-2 w-11/12 xl:h-96"
-				initial={{ y: "2.5rem" }}
-				animate={{ y: 0 }}
-				transition={{ duration: 0.75 }}
+				// initial={{ y: "2.5rem" }}
+				// animate={{ y: 0 }}
+				// transition={{ duration: 0.75 }}
 			>
 				<div className="_greeting-card-portrait-wrapper basis-2/5 flex h-full items-end justify-cent lg:ml-12 md:ml-6 ml-2 sm:ml-4">
 					{greetingCardPortraitImg()}
@@ -37,10 +35,10 @@ function greetingCard() {
 				<div className="_greeting-card-text basis-3/5 flex flex-col font-bold font-display lg:ml-12 md:ml-6 ml-2 translate-y-4">
 					<motion.h1>
 						<motion.div
-							initial={{ opacity: 0 }}
-							animate={{ opacity: 1 }}
-							transition={{ delay: 0.5, duration: 0.5 }}
 							className="_greeting-card-text-info lg:mt-8 mt-2 sm:mt-4"
+							// initial={{ opacity: 0 }}
+							// animate={{ opacity: 1 }}
+							// transition={{ delay: 0.5, duration: 0.5 }}
 						>
 							<div className="_greeting-card-text-info-iam dark:shadow-d-bg-light dark:text-gray-300 lg:text-2xl md:text-lg shadow-gray-200 sm:text-sm text-gray-500 text-shadow-lg text-xs">
 								<span lang="vn">Mình là</span>
